7ce50d3e1712de925b66e687ebea1b6ca04d7477
TheHunkNeil <bosaisun@gmail.com>
2021-11-06 11:29:38 +0100

    Site updated: 2021-11-06 11:29:37

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-window.addEventListener("DOMContentLoaded", function() {
-  const html            = document.querySelector("html");
-  const navBtn          = document.querySelector(".navbar-btn");
-  const navList         = document.querySelector(".navbar-list");
-  const backToTopFixed  = document.querySelector(".back-to-top-fixed");
-  let lastTop           = 0;
-  let theme             = window.localStorage.getItem('theme') || '';
+window.addEventListener("DOMContentLoaded", function () {
+  const html = document.querySelector("html");
+  const navBtn = document.querySelector(".navbar-btn");
+  const navList = document.querySelector(".navbar-list");
+  const backToTopFixed = document.querySelector(".back-to-top-fixed");
+  let lastTop = 0;
+  let theme = window.localStorage.getItem('theme') || '';
 
   theme && html.classList.add(theme)
 
@@ -46,7 +46,7 @@ window.addEventListener("DOMContentLoaded", function() {
   })
 
   // theme auto click
-  document.querySelector('#theme-auto').addEventListener('click', function() {
+  document.querySelector('#theme-auto').addEventListener('click', function () {
     html.classList.remove('theme-light')
     html.classList.remove('theme-dark')
     window.localStorage.setItem('theme', '')
@@ -84,28 +84,28 @@ window.addEventListener("DOMContentLoaded", function() {
  *
  * @returns 距离高度
  */
-function getScrollTop () {
+function getScrollTop() {
   return window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
 }
 
-function querySelectorArrs (selector) {
+function querySelectorArrs(selector) {
   return Array.from(document.querySelectorAll(selector))
 }
 
 
-function handleLazyBG () {
+function handleLazyBG() {
   const lazyBackgrounds = querySelectorArrs('[background-image-lazy]')
   let lazyBackgroundsCount = lazyBackgrounds.length
   if (lazyBackgroundsCount > 0) {
-    let lazyBackgroundObserver = new IntersectionObserver(function(entries, observer) {
-      entries.forEach(function({ isIntersecting, target }) {
+    let lazyBackgroundObserver = new IntersectionObserver(function (entries, observer) {
+      entries.forEach(function ({ isIntersecting, target }) {
         if (isIntersecting) {
           let img = target.dataset.img
           if (img) {
             target.style.backgroundImage = `url(${img})`
           }
           lazyBackgroundObserver.unobserve(target)
-          lazyBackgroundsCount --
+          lazyBackgroundsCount--
         }
         if (lazyBackgroundsCount <= 0) {
           lazyBackgroundObserver.disconnect()
@@ -113,8 +113,11 @@ function handleLazyBG () {
       })
     })
 
-    lazyBackgrounds.forEach(function(lazyBackground) {
+    lazyBackgrounds.forEach(function (lazyBackground) {
       lazyBackgroundObserver.observe(lazyBackground)
     })
   }
 }
+
+/*网页鼠标点击特效（爱心）*/
+!function (e, t, a) { function r() { for (var e = 0; e < s.length; e++) s[e].alpha <= 0 ? (t.body.removeChild(s[e].el), s.splice(e, 1)) : (s[e].y--, s[e].scale += .004, s[e].alpha -= .013, s[e].el.style.cssText = "left:" + s[e].x + "px;top:" + s[e].y + "px;opacity:" + s[e].alpha + ";transform:scale(" + s[e].scale + "," + s[e].scale + ") rotate(45deg);background:" + s[e].color + ";z-index:99999"); requestAnimationFrame(r) } function n() { var t = "function" == typeof e.onclick && e.onclick; e.onclick = function (e) { t && t(), o(e) } } function o(e) { var a = t.createElement("div"); a.className = "heart", s.push({ el: a, x: e.clientX - 5, y: e.clientY - 5, scale: 1, alpha: 1, color: c() }), t.body.appendChild(a) } function i(e) { var a = t.createElement("style"); a.type = "text/css"; try { a.appendChild(t.createTextNode(e)) } catch (t) { a.styleSheet.cssText = e } t.getElementsByTagName("head")[0].appendChild(a) } function c() { return "rgb(" + ~~(255 * Math.random()) + "," + ~~(255 * Math.random()) + "," + ~~(255 * Math.random()) + ")" } var s = []; e.requestAnimationFrame = e.requestAnimationFrame || e.webkitRequestAnimationFrame || e.mozRequestAnimationFrame || e.oRequestAnimationFrame || e.msRequestAnimationFrame || function (e) { setTimeout(e, 1e3 / 60) }, i(".heart{width: 10px;height: 10px;position: fixed;background: #f00;transform: rotate(45deg);-webkit-transform: rotate(45deg);-moz-transform: rotate(45deg);}.heart:after,.heart:before{content: '';width: inherit;height: inherit;background: inherit;border-radius: 50%;-webkit-border-radius: 50%;-moz-border-radius: 50%;position: fixed;}.heart:after{top: -5px;}.heart:before{left: -5px;}"), n(), r() }(window, document);
